da2a9fe17f771cc788c3088694c6aaa0aaa271cf
Andrea Maria Varisano <andreamaria.varisano@studio.unibo.it>
2023-06-11 19:29:16 +0200

    Add tests for boids' functions

--- boids_tests.cpp
+++ boids_tests.cpp
@@ -3,62 +3,50 @@
 #include "doctest.h"
 #include "flock.hpp"
 
-TEST_CASE("Testing the Boid and Flock classes") {
+TEST_CASE("Testing the Boid class and functions") {
   SUBCASE("Testing the Boid::update_state method") {
-    std::valarray<double> pos{2, 2};
-    std::valarray<double> vel{2, 2};
-    std::valarray<double> delta_vel{1, 1};
+    std::valarray<double> pos{2., 2.};
+    std::valarray<double> vel{2., 2.};
+    std::valarray<double> delta_vel{1., 1.};
 
     Boid boid(pos, vel);
-    boid.update_state(1, delta_vel);
+    boid.update_state(1., delta_vel);
 
-    CHECK(boid.get_vel()[0] == 3);
-    CHECK(boid.get_vel()[1] == 3);
-    CHECK(boid.get_pos()[0] == 5);
-    CHECK(boid.get_pos()[1] == 5);
+    CHECK(boid.get_vel()[0] == doctest::Approx(3.));
+    CHECK(boid.get_vel()[1] == doctest::Approx(3.));
+    CHECK(boid.get_pos()[0] == doctest::Approx(5.));
+    CHECK(boid.get_pos()[1] == doctest::Approx(5.));
+    CHECK(boid.get_angle() == doctest::Approx(45.));
   }
 
   SUBCASE("Testing the Boid::update_state method") {
-    std::valarray<double> pos{2, 2};
-    std::valarray<double> vel{1, 1};
-    std::valarray<double> delta_vel{0, 0};
+    std::valarray<double> pos{2., 2.};
+    std::valarray<double> vel{1., 1.};
+    std::valarray<double> delta_vel{0., 0.};
 
     Boid boid(pos, vel);
-    boid.update_state(1, delta_vel);
+    boid.update_state(1., delta_vel);
 
-    CHECK(boid.get_vel()[0] == 1);
-    CHECK(boid.get_vel()[1] == 1);
-    CHECK(boid.get_pos()[0] == 3);
-    CHECK(boid.get_pos()[1] == 3);
+    CHECK(boid.get_vel()[0] == doctest::Approx(1.));
+    CHECK(boid.get_vel()[1] == doctest::Approx(1.));
+    CHECK(boid.get_pos()[0] == doctest::Approx(3.));
+    CHECK(boid.get_pos()[1] == doctest::Approx(3.));
+    CHECK(boid.get_angle() == doctest::Approx(45.));
   }
 
   SUBCASE("Testing the Boid::update_state method") {
-    std::valarray<double> pos{-3, 2};
-    std::valarray<double> vel{5, -4};
-    std::valarray<double> delta_vel{0, -1};
+    std::valarray<double> pos{-3., 2.};
+    std::valarray<double> vel{5., -4.};
+    std::valarray<double> delta_vel{0., -1.};
 
     Boid boid(pos, vel);
-    boid.update_state(1, delta_vel);
+    boid.update_state(1., delta_vel);
 
-    CHECK(boid.get_vel()[0] == 5);
-    CHECK(boid.get_vel()[1] == -5);
-    CHECK(boid.get_pos()[0] == 2);
-    CHECK(boid.get_pos()[1] == -3);
-  }
-
-  SUBCASE("Testing the Boid::boid_dist method") {
-    Boid bd_1(2, 2, 5, 4);
-    Boid bd_2(4, 4, 1, -3);
-    Boid bd_3(0, 0, 3, 5);
-    Boid bd_4(0, 0, 6, -1);
-
-    double d12 = boid_dist(bd_1, bd_2);
-    double d13 = boid_dist(bd_1, bd_3);
-    double d34 = boid_dist(bd_3, bd_4);
-
-    CHECK(d12 == doctest::Approx(2.828427));
-    CHECK(d13 == doctest::Approx(2.828427));
-    CHECK(d34 == 0);
+    CHECK(boid.get_vel()[0] == doctest::Approx(5.));
+    CHECK(boid.get_vel()[1] == doctest::Approx(-5.));
+    CHECK(boid.get_pos()[0] == doctest::Approx(2.));
+    CHECK(boid.get_pos()[1] == doctest::Approx(-3.));
+    CHECK(boid.get_angle() == doctest::Approx(135.));
   }
 
   SUBCASE("Testing the vec_norm function") {
@@ -78,6 +66,52 @@ TEST_CASE("Testing the Boid and Flock classes") {
     CHECK(norm4 == doctest::Approx(4.1231056));
   }
 
+  SUBCASE("Testing the boid_dist function") {
+    Boid bd_1(2, 2, 5, 4);
+    Boid bd_2(4, 4, 1, -3);
+    Boid bd_3(0, 0, 3, 5);
+    Boid bd_4(0, 0, 6, -1);
+    Boid bd_5(1, 8, 0, 0);
+
+    double d12 = boid_dist(bd_1, bd_2);
+    double d13 = boid_dist(bd_1, bd_3);
+    double d34 = boid_dist(bd_3, bd_4);
+    double d15 = boid_dist(bd_1, bd_5);
+
+    CHECK(d12 == doctest::Approx(2.828427));
+    CHECK(d13 == doctest::Approx(2.828427));
+    CHECK(d34 == 0);
+    CHECK(d15 == doctest::Approx(6.082762));
+  }
+
+  SUBCASE("Testing the compute_angle function") {
+    std::valarray<double> vec_1{1, 4};
+    std::valarray<double> vec_2{1, -4};
+    std::valarray<double> vec_3{-1, -4};
+
+    double angle_1 = compute_angle<double>(vec_1);
+    double angle_2 = compute_angle<double>(vec_2);
+    double angle_3 = compute_angle<double>(vec_3);
+
+    CHECK(angle_1 == doctest::Approx(14.036243));
+    CHECK(angle_2 == doctest::Approx(165.963756));
+    CHECK(angle_3 == doctest::Approx(194.036243));
+  }
+
+  SUBCASE("Testing the is_visible function") {
+    Boid b1(1., 2., 3., 4.);
+    Boid b2(4., 3., 2., 1.);
+    Boid b3(1., 1., 1., 1.);
+
+    double angle{150.};
+    bool iv12 = is_visible(b1, b2, angle);
+    bool iv13 = is_visible(b1, b3, angle);
+    CHECK(iv12 == false);
+    CHECK(iv13 == true);
+  }
+}
+
+TEST_CASE("Testing the Flock class and functions") {
   SUBCASE("Testing the Flock::update_com method") {
     Boid bd_1(2, 2, 5, 4);
     Boid bd_2(5, 7, 1, -3);
