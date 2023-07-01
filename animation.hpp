8903c36879515fce23b12606074c4db47d11232e
Matteo Mirabelli <mirabellimatteo@gmail.com>
2023-07-01 17:46:44 +0200

    nothing important

--- animation.hpp
+++ animation.hpp
@@ -2,16 +2,17 @@
 #define ANIMATION_HPP
 
 #include <SFML/Graphics.hpp>
+#include <valarray>
 #include <vector>
 
 class Animate : public sf::Drawable, public sf::Transformable {
-  virtual void draw(sf::RenderTarget& target, sf::RenderStates states) const {
-    target.draw(a_sprite, states);
-  }
   float a_scale;
   int a_state;
   std::vector<sf::Texture> a_textures;
   sf::Sprite a_sprite;
+
+ protected:
+  virtual void draw(sf::RenderTarget&, sf::RenderStates) const;
 
  public:
   Animate(sf::Texture const&);
@@ -25,6 +26,47 @@ class Animate : public sf::Drawable, public sf::Transformable {
   void animate();
 };
 
-bool rec_contains(sf::FloatRect const&, sf::FloatRect const&);
+class Tracker : public sf::Drawable, public sf::Transformable {
+  sf::RectangleShape t_outer;
+  sf::RectangleShape t_inner;
+  sf::CircleShape t_circle;
+  sf::VertexArray t_path;
+  std::valarray<float> t_range;
+  std::valarray<float> t_pos;
+
+ protected:
+  virtual void draw(sf::RenderTarget&, sf::RenderStates) const;
+
+ public:
+  Tracker(std::valarray<float> const&, std::valarray<float> const&, float,
+          float);
+  void setPosition(sf::Vector2f const&);
+  void setFillColors(sf::Color const&, sf::Color const&, sf::Color const&);
+  void setOutlineColors(sf::Color const&, sf::Color const&, sf::Color const&);
+  void setOutlineThickness(float const&, float const&, float const&);
+  void update_pos(std::valarray<float> const&);
+  sf::RectangleShape const& getOuter() const;
+};
+
+class StatusBar : public sf::Drawable, public sf::Transformable {
+  sf::Text s_text;
+  sf::RectangleShape s_outer;
+  sf::RectangleShape s_bar;
+  std::valarray<float> s_range;
+  float s_value;
+
+ protected:
+  virtual void draw(sf::RenderTarget&, sf::RenderStates) const;
+
+ public:
+  StatusBar(std::string const&, sf::Font const&, float, float,
+            std::valarray<float> const&);
+  void setPosition(sf::Vector2f const&);
+  void setColors(sf::Color const&, sf::Color const&);
+  void setOutlineThickness(float);
+  void setRange(std::valarray<float> const&);
+  void update_value(float);
+  void set_text(std::string const&);
+};
 
 #endif
